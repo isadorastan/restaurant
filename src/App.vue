1affe563faa38f69de84678662997cc34160f6a2
Isadora Stangarlin <isadora.stangarlin@AC-000757-lucasabrao.local>
2021-05-30 22:46:55 -0300

    Category menu desktop

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
     <div id="app">
-        <img alt="Vue logo" src="./assets/logo.png" />
-        <HelloWorld msg="Welcome to Your Vue.js App" />
+        <CategoryMenu/>
     </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
+import CategoryMenu from './components/CategoryMenu.vue';
 
 export default {
     name: 'App',
     components: {
-        HelloWorld
+        CategoryMenu
     }
 };
 </script>
---
+++ src/components/CategoryMenu.vue
@@ -0,0 +1,107 @@
+<template>
+    <div class="category-menu">
+        <ul>
+            <li
+                v-for="category in categoriesList"
+                :key="category.id"
+                @click="onCategoryClick(category.id)"
+                :class="{ active: isActive(category.id) }"
+            >
+                <component :is="category.icon" />
+                <p>{{ category.label }}</p>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+import Pizza from '../assets/icons/pizza.svg';
+import Combo from '../assets/icons/french-fries.svg';
+import Deserts from '../assets/icons/ice-cream.svg';
+import Drinks from '../assets/icons/smothie.svg';
+import Burguer from '../assets/icons/food.svg';
+
+export default {
+    name: 'CategoryMenu',
+    components: {
+        Pizza,
+        Combo,
+        Deserts,
+        Drinks,
+        Burguer
+    },
+    data() {
+        return {
+            categoriesList: [
+                { label: 'Pizza', icon: 'Pizza', id: 'pizza' },
+                { label: 'Bebidas', icon: 'Drinks', id: 'drinks' },
+                { label: 'Doces', icon: 'Deserts', id: 'deserts' },
+                { label: 'Combos', icon: 'Combo', id: 'combo' },
+                { label: 'Burguers', icon: 'Burguer', id: 'burguer' }
+            ],
+            selectedCategory: 'pizza'
+        };
+    },
+    methods: {
+        onCategoryClick(id) {
+            this.selectedCategory = id;
+        },
+        isActive(id) {
+            return this.selectedCategory === id;
+        }
+    }
+};
+</script>
+
+<style lang="less" scoped>
+.category-menu {
+    width: 130px;
+    height: 100vh;
+    background: white;
+
+    display: flex;
+    align-items: center;
+
+    ul {
+        list-style: none;
+        padding: 0;
+        width: 100%;
+
+        li {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            justify-content: center;
+            height: 100px;
+
+            p {
+                margin-bottom: 0px;
+                font-weight: 500;
+                font-size: 14px;
+                color: @dark-grey;
+            }
+
+            svg {
+                path {
+                    fill: @dark-grey;
+                }
+            }
+
+            &.active {
+                background: @yellow;
+                border-radius: 8px;
+
+                p {
+                    color: black;
+                }
+
+                svg {
+                    path {
+                        fill: black;
+                    }
+                }
+            }
+        }
+    }
+}
+</style>
