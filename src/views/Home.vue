0453e6e941d30594515eb453f005926bbcc9a3f1
Isadora Stangarlin <isadora.stangarlin@AC-000757-lucasabrao.local>
2021-05-31 21:00:18 -0300

    Json server

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,11 +7,17 @@
 <script>
 // @ is an alias to /src
 import CategoryMenu from '@/components/CategoryMenu.vue'
+import axios from 'axios';
 
 export default {
   name: 'Home',
   components: {
     CategoryMenu
+  },
+  created() {
+    axios.get('http://localhost:3000/burguers').then( response => {
+      console.log(response);
+    })
   }
 }
 </script>
