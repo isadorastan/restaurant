7104832153a6e8cd47f1072b266a94cb6757903c
Isadora Stangarlin <isadora.stangarlin@teste.local>
2021-06-26 18:29:14 -0300

    Terminando os seuss dados

--- src/components/Order.vue
+++ src/components/Order.vue
@@ -1,15 +1,32 @@
 <template>
     <div class="order">
         <form>
-            <label for="">{{ formData.name.label }}</label>
-            <input
-                type="text"
-                placeholder="Digite seu nome"
-                v-model="formData.name.value"
-                @blur="formData.name.isValid()"
-                :class="{'error' : !formData.name.valid}"
-            />
-            <p class="error-message" v-if="!formData.name.valid">{{ formData.name.errorMessage }}</p>
+            <p class="section-title">Seus dados</p>
+
+            <div class="input-field">
+                <label for="">{{ formData.name.label }}</label>
+                <input
+                    type="text"
+                    :placeholder="formData.name.placeholder"
+                    v-model="formData.name.value"
+                    @blur="formData.name.isValid()"
+                    :class="{ error: !formData.name.valid }"
+                />
+                <p class="error-message" v-if="!formData.name.valid">{{ formData.name.errorMessage }}</p>
+            </div>
+
+            <div class="input-field">
+                <label for="">{{ formData.cellphone.label }}</label>
+                <input
+                    type="text"
+                    :placeholder="formData.cellphone.placeholder"
+                    v-mask="'(##) # ####-####'"
+                    v-model="formData.cellphone.value"
+                    @blur="formData.cellphone.isValid()"
+                    :class="{ error: !formData.cellphone.valid }"
+                />
+                <p class="error-message" v-if="!formData.cellphone.valid">{{ formData.cellphone.errorMessage }}</p>
+            </div>
         </form>
         <button class="primary-button" @click="orderItens">Concluir pedido</button>
     </div>
@@ -22,11 +39,22 @@ export default {
             formData: {
                 name: {
                     value: '',
+                    placeholder: 'Digite seu nome',
                     errorMessage: 'O nome é obrigatório',
                     label: 'Nome*',
                     valid: true,
                     isValid: () => {
                         this.formData.name.valid = this.formData.name.value.length;
+                    }
+                },
+                cellphone: {
+                    value: '',
+                    placeholder: 'Digite seu celular',
+                    errorMessage: 'O celular é obrigatório',
+                    label: 'Celular*',
+                    valid: true,
+                    isValid: () => {
+                      this.formData.cellphone.valid = (this.formData.cellphone.value.length === 16);
                     }
                 }
             }
@@ -35,6 +63,7 @@ export default {
     methods: {
         triggerValidations() {
             this.formData.name.isValid();
+            this.formData.cellphone.isValid();
         },
         orderItens() {
             this.triggerValidations();
@@ -55,6 +84,21 @@ export default {
         display: flex;
         flex-direction: column;
 
+        .input-field {
+            display: flex;
+            flex-direction: column;
+
+            & + .input-field {
+              margin-top: 15px;
+            }
+        }
+
+        .section-title {
+          font-weight: 600;
+          font-size: 22px;
+          margin-bottom: 20px;
+        }
+
         label {
             font-weight: 500;
             font-size: 16px;
@@ -65,6 +109,10 @@ export default {
             font-size: 12px;
             color: @error-color;
         }
+    }
+
+    button {
+        margin: 30px auto;
     }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -5,7 +5,9 @@ import './assets/css/global.less'
 import router from './router'
 import 'es6-promise/auto'
 import { store } from './store/store'
+import VueMask from 'v-mask'
 
+Vue.use(VueMask);
 Vue.use(Vuex) 
 
 Vue.config.productionTip = false
