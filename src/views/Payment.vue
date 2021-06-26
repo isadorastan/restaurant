f4f88fc06b9c92f4133c7e1d1ff39c4eb42fa116
Isadora Stangarlin <isadora.stangarlin@teste.local>
2021-06-26 15:56:44 -0300

    payment view

--- src/views/Payment.vue
+++ src/views/Payment.vue
@@ -1,15 +1,42 @@
 <template>
-  <div>
-      pagamento
+  <div class="payment">
+      <div class="payment--content">
+        <router-link to="/" class="payment--go-back">←️ Voltar</router-link>
+      </div>
+      <Cart v-if="isDesktop()" />
   </div>
 </template>
 
 <script>
-export default {
 
+import Mixin from '@/mixins/mixins';
+import Cart from '@/components/Cart.vue';
+
+export default {
+  components: {
+    Cart
+  },
+  mixins: [Mixin]
 }
 </script>
 
-<style>
+<style lang="less" scoped>
+  .payment {
+    display: flex;
+
+    &--content {
+      flex-grow: 1;
+      padding: 50px;
+      
+      .payment--go-back {
+          font-weight: 600;
+          font-size: 18px;
+          text-decoration: none;
+          color: black;
+      }
+    }
+
+
+  }
 
 </style>
