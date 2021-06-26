7e6c8fe0bddc8cf157b95da5a9b18e806077b095
Isadora Stangarlin <isadora.stangarlin@teste.local>
2021-06-26 19:15:48 -0300

    address section 1

--- src/components/Order.vue
+++ src/components/Order.vue
@@ -1,31 +1,47 @@
 <template>
     <div class="order">
         <form>
-            <p class="section-title">Seus dados</p>
-
-            <div class="input-field">
-                <label for="">{{ formData.name.label }}</label>
-                <input
-                    type="text"
-                    :placeholder="formData.name.placeholder"
-                    v-model="formData.name.value"
-                    @blur="formData.name.isValid()"
-                    :class="{ error: !formData.name.valid }"
-                />
-                <p class="error-message" v-if="!formData.name.valid">{{ formData.name.errorMessage }}</p>
+            <div class="user-data">
+                <p class="section-title">Seus dados</p>
+                <div class="input-field">
+                    <label for="">{{ formData.name.label }}</label>
+                    <input
+                        type="text"
+                        :placeholder="formData.name.placeholder"
+                        v-model="formData.name.value"
+                        @blur="formData.name.isValid()"
+                        :class="{ error: !formData.name.valid }"
+                    />
+                    <p class="error-message" v-if="!formData.name.valid">{{ formData.name.errorMessage }}</p>
+                </div>
+                <div class="input-field">
+                    <label for="">{{ formData.cellphone.label }}</label>
+                    <input
+                        type="text"
+                        :placeholder="formData.cellphone.placeholder"
+                        v-mask="'(##) # ####-####'"
+                        v-model="formData.cellphone.value"
+                        @blur="formData.cellphone.isValid()"
+                        :class="{ error: !formData.cellphone.valid }"
+                    />
+                    <p class="error-message" v-if="!formData.cellphone.valid">{{ formData.cellphone.errorMessage }}</p>
+                </div>
             </div>
 
-            <div class="input-field">
-                <label for="">{{ formData.cellphone.label }}</label>
-                <input
-                    type="text"
-                    :placeholder="formData.cellphone.placeholder"
-                    v-mask="'(##) # ####-####'"
-                    v-model="formData.cellphone.value"
-                    @blur="formData.cellphone.isValid()"
-                    :class="{ error: !formData.cellphone.valid }"
-                />
-                <p class="error-message" v-if="!formData.cellphone.valid">{{ formData.cellphone.errorMessage }}</p>
+            <div class="address">
+              <p class="section-title">Endereço</p>
+              <div class="delivery-type">
+                <div class="radio-options">
+                  <input type="radio" name="delivery-type" id="store" checked>
+                  <label for="store">Retirar na loja</label>
+                </div>
+
+                <div class="radio-options">
+                  <input type="radio" name="delivery-type" id="delivery">
+                  <label for="delivery">Delivery</label>
+                </div>
+              </div>
+              <a>Adicionar endereço</a>
             </div>
         </form>
         <button class="primary-button" @click="orderItens">Concluir pedido</button>
@@ -54,7 +70,7 @@ export default {
                     label: 'Celular*',
                     valid: true,
                     isValid: () => {
-                      this.formData.cellphone.valid = (this.formData.cellphone.value.length === 16);
+                        this.formData.cellphone.valid = this.formData.cellphone.value.length === 16;
                     }
                 }
             }
@@ -89,14 +105,14 @@ export default {
             flex-direction: column;
 
             & + .input-field {
-              margin-top: 15px;
+                margin-top: 15px;
             }
         }
 
         .section-title {
-          font-weight: 600;
-          font-size: 22px;
-          margin-bottom: 20px;
+            font-weight: 600;
+            font-size: 22px;
+            margin-bottom: 20px;
         }
 
         label {
@@ -108,6 +124,35 @@ export default {
         .error-message {
             font-size: 12px;
             color: @error-color;
+        }
+
+
+        .address {
+          .delivery-type {
+            display: flex;
+          }
+
+          a {
+            color: @pink;
+            font-weight: normal;
+            font-size: 12px;
+            text-decoration: underline;
+            cursor: pointer;
+          }
+        }
+
+        .radio-options {
+          display: flex;
+          align-items: center;   
+
+          &+.radio-options {
+            margin-left: 25px;
+          }
+
+          label {
+            padding-left: 10px;
+            margin: 0;
+          }
         }
     }
 
